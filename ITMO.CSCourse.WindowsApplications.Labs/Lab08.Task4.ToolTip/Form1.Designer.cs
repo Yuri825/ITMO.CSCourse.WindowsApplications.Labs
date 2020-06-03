1e9d2dbf8c868bc8e3d66b46f75c53d4751d03ec
EgorAlekseev <54812985+EgorAlekseev@users.noreply.github.com>
2020-06-03 15:20:35 +0300

    Add files

---
+++ ITMO.CSCourse.WindowsApplications.Labs/Lab08.Task4.ToolTip/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Lab07.Task3.WinAsynchMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.txbA = new System.Windows.Forms.TextBox();
+            this.txbB = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnWork = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(12, 21);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(65, 13);
+            this.lblA.TabIndex = 0;
+            this.lblA.Text = "Значение А";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(220, 21);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(65, 13);
+            this.lblB.TabIndex = 1;
+            this.lblB.Text = "Значение В";
+            // 
+            // txbA
+            // 
+            this.txbA.Location = new System.Drawing.Point(92, 18);
+            this.txbA.Name = "txbA";
+            this.txbA.Size = new System.Drawing.Size(100, 20);
+            this.txbA.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.txbA, "For input integer A");
+            // 
+            // txbB
+            // 
+            this.txbB.Location = new System.Drawing.Point(297, 18);
+            this.txbB.Name = "txbB";
+            this.txbB.Size = new System.Drawing.Size(100, 20);
+            this.txbB.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.txbB, "For input integer B");
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(15, 58);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 4;
+            this.btnRun.Text = "Сумма";
+            this.toolTip1.SetToolTip(this.btnRun, "Sum");
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnWork
+            // 
+            this.btnWork.Location = new System.Drawing.Point(156, 107);
+            this.btnWork.Name = "btnWork";
+            this.btnWork.Size = new System.Drawing.Size(75, 23);
+            this.btnWork.TabIndex = 5;
+            this.btnWork.Text = "Работа";
+            this.toolTip1.SetToolTip(this.btnWork, "Start work");
+            this.btnWork.UseVisualStyleBackColor = true;
+            this.btnWork.Click += new System.EventHandler(this.btnWork_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 161);
+            this.Controls.Add(this.btnWork);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txbB);
+            this.Controls.Add(this.txbA);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Name = "Form1";
+            this.Text = "Асинхронный запуск";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.TextBox txbA;
+        private System.Windows.Forms.TextBox txbB;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnWork;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
